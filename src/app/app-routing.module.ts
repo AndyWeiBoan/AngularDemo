cbbe0c8e2a296377c4a3c198eff66a072d2015b4
ANDY <hhk8056tw@gmail.com>
2018-05-20 21:34:31 +0800

    routing setting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MessageComponent } from './message/message.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component: HomeComponent},
+  { path: 'Message', component: MessageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
